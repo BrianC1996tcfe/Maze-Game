5cae1c150c4a65e9de4e1e8e63b705bb24c16298
BrianC1996tcfe <58697088+BrianC1996tcfe@users.noreply.github.com>
2020-01-29 17:42:03 +0000

    Door Opens When standing on Red Square Then Closes after 5 seconds

--- Games Fleadh Maze Game/Assets/Expo-rt/Proto2/SceneScripts/PressurePlateTrigger.cs
+++ Games Fleadh Maze Game/Assets/Expo-rt/Proto2/SceneScripts/PressurePlateTrigger.cs
@@ -6,18 +6,62 @@ public class PressurePlateTrigger : MonoBehaviour {
 
 	public GameObject moveDoor;
 	public GameObject movePressurePlate;
+	private Vector3 DoorStartPos,DoorPosNow;
+	private bool OnPressurePlate,timerGo;
+	private float timeLeft;
 
-
+	void Start()
+	{
+		timerGo = false;
+		OnPressurePlate = false;
+		timeLeft = 5;
+        Vector3 position = moveDoor.transform.position;
+        DoorStartPos = position;
+		DoorPosNow = position;
+		//Debug.Log("WEE "+DoorStartPos);
+	}
+	void Update()
+	{
+		if(!OnPressurePlate && timerGo)
+		{
+			if(timeLeft>0)
+			{
+				timeLeft -= Time.deltaTime;
+				Debug.Log("Timer "+ timeLeft + timerGo);
+			}
+			if( timeLeft <= 0 && (!(DoorPosNow.y >= DoorStartPos.y)))
+			{
+				//timerGo = false;
+				DoorPosNow.y = DoorPosNow.y + 0.1f;
+				moveDoor.transform.position = DoorPosNow;
+				//Debug.Log("WNN "+ DoorPosNow);
+				//Debug.Log("Timer "+ timeLeft);
+			}
+			else if(timeLeft <= 0)
+			{
+				timerGo = false;
+			}
+		}
+	}
 	private void OnTriggerStay()
 	{
-		moveDoor.transform.position += moveDoor.transform.forward * Time.deltaTime*5;
+		if(DoorPosNow.y >= -41){
+		DoorPosNow.y = DoorPosNow.y - 0.1f;
+		//Debug.Log("WAA "+ DoorPosNow);
+		//moveDoor.transform.position += moveDoor.transform.forward * Time.deltaTime*5;
+		moveDoor.transform.position = DoorPosNow;
+		}
 	}
 	private void OnTriggerEnter()
 	{
-		movePressurePlate.transform.localPosition = new Vector3 (250, -1.5f, 280);
+		OnPressurePlate = true;
+		timeLeft = 5;
+		movePressurePlate.transform.position = new Vector3 (250, -0.5f, 280);
 	}
 	private void OnTriggerExit()
 	{
-		movePressurePlate.transform.localPosition = new Vector3 (250, 0, 280);
+		timerGo = true;
+		OnPressurePlate=false;
+		movePressurePlate.transform.position = new Vector3 (250, 0, 280);
 	}
 }
