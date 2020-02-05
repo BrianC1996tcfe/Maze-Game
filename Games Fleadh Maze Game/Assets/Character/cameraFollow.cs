9af99d874b32d5c956d4ef2c464d29ccfdb76654
ddzik19 <ddzik09@gmail.com>
2020-02-05 17:58:32 +0000

    Player + Skeleton
    Completely changed the player movement mechanic (not able to attack YET) and added a new enemy with full animations and damage script.

---
+++ Games Fleadh Maze Game/Assets/Character/cameraFollow.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour {
+
+	public float cameraMoveSpeed = 120.0f;
+	public GameObject cameraFollowObject;
+	Vector3 FollowPos;
+	public float clampAngle = 80.0f;
+	public float inputSensitivity = 150.0f;
+	public GameObject camera;
+	public GameObject player;
+	public float camDistanceXToPlayer;
+	public float camDistanceYToPlayer;
+	public float camDistanceZToPlayer;
+	public float mouseX;
+	public float mouseY;
+	public float finalInputX;
+	public float finalInputZ;
+	public float smoothX;
+	public float smoothY;
+	public float rotY;
+	public float rotX;
+
+
+
+
+	void Start () {
+		Vector3 rot = transform.localRotation.eulerAngles;
+		rotY = rot.y;
+		rotX = rot.x;
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+	
+
+	void Update () {
+		float inputX= Input.GetAxis("RightStickHorizontal");
+		float inputZ= Input.GetAxis("RightStickVertical");
+		mouseX = Input.GetAxis ("Mouse X");
+		mouseY = Input.GetAxis ("Mouse Y");
+		finalInputX = inputX + mouseX;
+		finalInputZ = inputZ + mouseY;
+
+		rotY += finalInputX * inputSensitivity * Time.deltaTime;
+		rotX += finalInputZ * inputSensitivity * Time.deltaTime;
+
+		rotX = Mathf.Clamp (rotX, -clampAngle, clampAngle);
+
+		Quaternion localRotation = Quaternion.Euler (rotX, rotY, 0.0f);
+		transform.rotation = localRotation;
+	}
+
+	void LateUpdate(){
+		CameraUpdater ();
+	}
+
+	void CameraUpdater(){
+		Transform target = cameraFollowObject.transform;
+
+		float step = cameraMoveSpeed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards (transform.position, target.position, step);
+	}
+}
---
+++ Games Fleadh Maze Game/Assets/Enemies/Skeleton W_Sword/swordDamage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swordDamage : MonoBehaviour {
+
+	public float dmg;
+
+	void OnTriggerEnter(Collider other){
+		other.gameObject.GetComponent<PlayerHealth> ().TakeDamage (dmg);
+	}
+}
