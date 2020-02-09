876b914d00f3af7024f427687f45bfbe6569d450
ddzik19 <ddzik09@gmail.com>
2020-02-09 14:21:15 +0000

    Destroyable objects + Script
    turned the barrel into a destroyable object (can also be undestroyable),created a vase prefab (also destroyable). Created a script which allows objects to be destroyed and spawn random items(could be money,gems,weapons)

---
+++ Games Fleadh Maze Game/Assets/Prefabs/Barrel/BarrelDestroy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelDestroy : MonoBehaviour {
+
+	float interval = 1f;
+
+	public Transform spawnPoint;
+
+	public GameObject broken;
+	public GameObject spawn1;
+	public GameObject spawn2;
+	public GameObject spawn3;
+
+	int n;
+
+	void OnTriggerEnter(){
+		n = Random.Range (0, 10);
+		print (n);
+
+		GameObject.Instantiate (broken, transform.position, Quaternion.identity);
+		Destroy (gameObject);
+
+		if(n <= 3){
+			Instantiate (spawn1, spawnPoint.position, Quaternion.identity);
+		}
+		if(n <= 6 && n >= 4){
+			Instantiate (spawn2, spawnPoint.position, Quaternion.identity);
+		}
+		if(n <= 10 && n >= 7){
+			Instantiate (spawn3, spawnPoint.position, Quaternion.identity);
+		}
+
+	}
+
+
+}
