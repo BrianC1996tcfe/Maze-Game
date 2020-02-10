2613743f62d318fac76ef6977d044a19c34e0ae4
ddzik19 <ddzik09@gmail.com>
2020-02-10 00:44:48 +0000

    inventory
    created a very basic inventory and started working on scripting it.

---
+++ Games Fleadh Maze Game/Assets/Character/Inventory/Slot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slot : MonoBehaviour {
+
+	public GameObject Item;
+	public bool empty;
+	public int ID;
+	public string Type;
+	public string Description;
+	public Sprite Icon;
+
+	public void UpdateSlot(){
+
+		this.GetComponent<>.sprite = Icon;
+	}
+}
