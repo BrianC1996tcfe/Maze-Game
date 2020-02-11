c25acd6db9dcd7b909d470c586e401f6f39b0c3b
ddzik19 <ddzik09@gmail.com>
2020-02-11 15:49:59 +0000

    Cleanup
    got rid of syuff we didnt need and celaned up folders a little bit

---
+++ Games Fleadh Maze Game/Assets/Scripts/DamageScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageScript : MonoBehaviour {
+
+	public float dmg;
+
+	void OnTriggerEnter(Collider other){
+		other.gameObject.GetComponent<PlayerHealth> ().TakeDamage (dmg);
+	}
+}
---
+++ Games Fleadh Maze Game/Assets/Scripts/DestroyObjects/DestroyOverTime.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOverTime : MonoBehaviour {
+
+	float interval = 10f;
+
+
+	void OnTriggerEnter(){
+		Destroy(gameObject,interval);
+	}
+
+}
