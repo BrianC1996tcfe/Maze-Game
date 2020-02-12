5524e7a6a40110c884e0967c66e60f4be38dc1ec
BrianC1996tcfe <58697088+BrianC1996tcfe@users.noreply.github.com>
2020-02-12 18:42:30 +0000

    Fixed Problem Made With last Commit
    I wasn't turning a script back on

--- Games Fleadh Maze Game/Assets/InventoryManager.cs
+++ Games Fleadh Maze Game/Assets/InventoryManager.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryManager : MonoBehaviour {
 	public GameObject playerPrefab;
 	public GameObject CameraPrefab;
     public GameObject ItemButton;
 	public GameObject InventoryCanvas;
+	public Button InvClosebutton;
 	public GameObject[] Items;
 	private int slotAmount = 25;
 	// private int row = 5;
@@ -22,7 +24,23 @@ public class InventoryManager : MonoBehaviour {
 		// CameraPrefab.GetComponent<cameraFollow>().enabled = false;
 		// //Disables player movement script 
 		// playerPrefab.GetComponent<NewMovement>().enabled = false;
-		//MakeButtonRows();
+		//MakeButtonRows();void Start () {
+		Button btn = InvClosebutton.GetComponent<Button>();
+		btn.onClick.AddListener(CloseInventory);
+	}
+	public void CloseInventory () {
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible=false;
+		InventoryCanvas.SetActive(false);
+		CameraPrefab.GetComponent<cameraFollow>().enabled = true;
+		playerPrefab.GetComponent<NewMovement>().enabled = true;
+	}	
+	public void OpenInventory () {
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible=true;
+		InventoryCanvas.SetActive(true);
+		CameraPrefab.GetComponent<cameraFollow>().enabled = false;
+		playerPrefab.GetComponent<NewMovement>().enabled = false;
 	}	
 	void Update () {
 		// if(InventoryCanvas.activeSelf){
@@ -33,18 +51,10 @@ public class InventoryManager : MonoBehaviour {
         {
 			switch(InventoryCanvas.activeSelf){
 				case true:
-					Cursor.lockState = CursorLockMode.Locked;
-					Cursor.visible=false;
-					InventoryCanvas.SetActive(false);
-					CameraPrefab.GetComponent<cameraFollow>().enabled = true;
-					playerPrefab.GetComponent<NewMovement>().enabled = true;
+					CloseInventory ();
 				break;
 				case false:
-					Cursor.lockState = CursorLockMode.None;
-					Cursor.visible=true;
-					InventoryCanvas.SetActive(true);
-					CameraPrefab.GetComponent<cameraFollow>().enabled = false;
-					playerPrefab.GetComponent<NewMovement>().enabled = false;
+					OpenInventory ();
 				break;
 			}
 		}
