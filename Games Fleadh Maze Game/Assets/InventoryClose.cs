ec17c7f4b84aa714ad934ebe7de64764ff66ec27
BrianC1996tcfe <58697088+BrianC1996tcfe@users.noreply.github.com>
2020-02-12 18:34:59 +0000

    Inventory Menu Close On Button Click

---
+++ Games Fleadh Maze Game/Assets/InventoryClose.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class InventoryClose : MonoBehaviour {
+ public GameObject InventoryMenu;
+ public Button Invbutton;
+	void Start () {
+		Button btn = Invbutton.GetComponent<Button>();
+		btn.onClick.AddListener(CloseInventory);
+	}
+	public void CloseInventory () {
+		InventoryMenu.SetActive(false);
+	}
+}
