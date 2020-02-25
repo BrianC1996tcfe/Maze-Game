0683ce2083eddcab61889ce9a04c5a05de9e0ddc
ddzik19 <ddzik09@gmail.com>
2020-02-25 18:01:32 +0000

    Enemy Manager
    created an enemy manager script.

--- Games Fleadh Maze Game/Assets/GameManager.cs
+++ Games Fleadh Maze Game/Assets/GameManager.cs
@@ -8,14 +8,6 @@ public GameObject Maze;
 //xp on death
 Vector3 MazePos;
 
-	public int level;
-	public int n;
-	public float hp;
-	public int fairy;
-	public int swordskele;
-	public int throwskele;
-
-	private GameObject enemy;
 
 
 	// Use this for initialization
@@ -23,7 +15,7 @@ Vector3 MazePos;
 		MazePos = Maze.transform.position;
 		GameObject newMaze = Instantiate(Maze,MazePos,Quaternion.identity);
 		newMaze.tag = "Maze";
-		enemy = GameObject.FindGameObjectWithTag ("Enemy");
+
 	}
 
 	// Update is called once per frame
@@ -32,56 +24,7 @@ Vector3 MazePos;
 		NextLevel();
 		}
 	}
-
-	public void EnemyLevel(){
-
-		level += n;
-
-		switch (level) {
-
-		case 1:
-			{
-				enemy.GetComponent<EnemyHealth> ().SetHealth (30);
-				enemy.GetComponent<Fireball> ().AddDamage (2);
-				enemy.GetComponent<swordDamage> ().AddDamage (2);
-				enemy.GetComponent<BoneScript> ().AddDamage (2);
-				break;
-			}
-		case 2:
-			{
-				enemy.GetComponent<EnemyHealth> ().SetHealth (75);
-				enemy.GetComponent<Fireball> ().AddDamage (5);
-				enemy.GetComponent<swordDamage> ().AddDamage (5);
-				enemy.GetComponent<BoneScript> ().AddDamage (5);
-				break;
-			}
-		case 3:
-			{
-				enemy.GetComponent<EnemyHealth> ().SetHealth (100);
-				enemy.GetComponent<Fireball> ().AddDamage (10);
-				enemy.GetComponent<swordDamage> ().AddDamage (10);
-				enemy.GetComponent<BoneScript> ().AddDamage (10);
-				break;
-			}
-		case 4:
-			{
-				enemy.GetComponent<EnemyHealth> ().SetHealth (150);
-				enemy.GetComponent<Fireball> ().AddDamage (15);
-				enemy.GetComponent<swordDamage> ().AddDamage (15);
-				enemy.GetComponent<BoneScript> ().AddDamage (15);
-				break;
-			}
-		case 5:
-			{
-				enemy.GetComponent<EnemyHealth> ().SetHealth (200);
-				enemy.GetComponent<Fireball> ().AddDamage (21);
-				enemy.GetComponent<swordDamage> ().AddDamage (21);
-				enemy.GetComponent<BoneScript> ().AddDamage (21);
-				break;
-			}
-		}
-	}
-
+		
 	public void NextLevel(){
 
 		// Maze.enemylevel++;
---
+++ Games Fleadh Maze Game/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour {
+
+	public int level;
+	public int n;
+	public float hp;
+	public int fairy;
+	public int swordskele;
+	public int throwskele;
+
+	private GameObject enemy;
+	// Use this for initialization
+	void Start () {
+		enemy = GameObject.FindGameObjectWithTag ("Enemy");
+	}
+
+	public void EnemyLevel(){
+
+		level += n;
+
+		switch (level) {
+
+		case 1:
+			{
+				enemy.GetComponent<EnemyHealth> ().SetHealth (30);
+				enemy.GetComponent<Fireball> ().AddDamage (2);
+				enemy.GetComponent<swordDamage> ().AddDamage (2);
+				enemy.GetComponent<BoneScript> ().AddDamage (2);
+				break;
+			}
+		case 2:
+			{
+				enemy.GetComponent<EnemyHealth> ().SetHealth (75);
+				enemy.GetComponent<Fireball> ().AddDamage (5);
+				enemy.GetComponent<swordDamage> ().AddDamage (5);
+				enemy.GetComponent<BoneScript> ().AddDamage (5);
+				break;
+			}
+		case 3:
+			{
+				enemy.GetComponent<EnemyHealth> ().SetHealth (100);
+				enemy.GetComponent<Fireball> ().AddDamage (10);
+				enemy.GetComponent<swordDamage> ().AddDamage (10);
+				enemy.GetComponent<BoneScript> ().AddDamage (10);
+				break;
+			}
+		case 4:
+			{
+				enemy.GetComponent<EnemyHealth> ().SetHealth (150);
+				enemy.GetComponent<Fireball> ().AddDamage (15);
+				enemy.GetComponent<swordDamage> ().AddDamage (15);
+				enemy.GetComponent<BoneScript> ().AddDamage (15);
+				break;
+			}
+		case 5:
+			{
+				enemy.GetComponent<EnemyHealth> ().SetHealth (200);
+				enemy.GetComponent<Fireball> ().AddDamage (21);
+				enemy.GetComponent<swordDamage> ().AddDamage (21);
+				enemy.GetComponent<BoneScript> ().AddDamage (21);
+				break;
+			}
+		}
+	}
+}
