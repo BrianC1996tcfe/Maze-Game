6313c94d8c47723c5ee46e3520e0a7c6a3a387d5
ddzik19 <ddzik09@gmail.com>
2020-02-28 17:49:34 +0000

    Skeleton King AGAIN
    added skeleton king boss AGAIN

--- Games Fleadh Maze Game/Assets/Art/Enemies/Skeleton W_Sword/swordDamage.cs
+++ Games Fleadh Maze Game/Assets/Art/Enemies/Skeleton W_Sword/swordDamage.cs
@@ -7,21 +7,11 @@ public class swordDamage : MonoBehaviour {
 	public int dmg = 5;
 
 	void OnTriggerEnter(Collider other){
-		StartCoroutine ("Collider");
 		if (other.gameObject.tag=="Player") {
 			other.gameObject.GetComponent<PlayerHealth> ().TakeDamage (dmg);
 		}
 	}
-
 	public void AddDamage(int amount){
 		dmg += amount;
-	}
-		
-	IEnumerator Collider(){
-		this.GetComponent<BoxCollider> ().enabled = false;
-
-		yield return new WaitForSeconds (1f);
-
-		this.GetComponent<BoxCollider> ().enabled = true;
 	}
 }
--- Games Fleadh Maze Game/Assets/Art/Enemies/SkeletonKing/SkeleKingScript.cs
+++ Games Fleadh Maze Game/Assets/Art/Enemies/SkeletonKing/SkeleKingScript.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SWSController : MonoBehaviour {
+public class SkeleKingScript : MonoBehaviour {
 
 	public Transform player;
 	static Animator anim;
+	private bool canAttack = false;
 
 	void Start(){
 		player = GameObject.FindGameObjectWithTag ("Player").transform;
 		anim = this.GetComponent<Animator>();
 	}
 	void Update () {
-		if (Vector3.Distance (player.position, this.transform.position) < 13) {
+		if (Vector3.Distance (player.position, this.transform.position) < 17) {
 			Vector3 direction = player.position - this.transform.position;
 			direction.y = 0;
 
@@ -20,19 +21,23 @@ public class SWSController : MonoBehaviour {
 
 			anim.SetBool ("isIdle", false);
 
-			if (direction.magnitude > 3) {
+			if (direction.magnitude > 5) {
 				//0.05f is the movement speed towards the player
-				this.transform.Translate (0, 0, 0.1f);
+				this.transform.Translate (0, 0, 0.2f);
 				anim.SetBool ("isWalking", true);
 				anim.SetBool ("isAttacking", false);
 			} else {
-				anim.SetBool ("isAttacking", true);
-				anim.SetBool ("isWalking", false);
+				Attack ();
 			}
 		} else {
 			anim.SetBool ("isIdle", true);
 			anim.SetBool ("isWalking", false);
 			anim.SetBool ("isAttacking", false);
 		}
+	}
+
+	void Attack (){
+		anim.SetBool ("isWalking", false);
+		anim.SetBool ("isAttacking", true);
 	}
 }
